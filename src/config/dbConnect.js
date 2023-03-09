78b5fed705ac1fa0781aa3c898a4f11e872c4fa0
Vini-dev-git <75448262+Vini-dev-git@users.noreply.github.com>
2023-03-09 00:07:37 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://root:root@bookstore.xq2hzsu.mongodb.net/bookStore-node');
+mongoose.connect('');
 
 let db = mongoose.connection;
 
